9b10e43ecdc77d161a09b60c17507602209192a3
A.D.Faris <adfaris@gmail.com>
2018-11-05 16:45:14 -0500

    fizzbuzz done

--- fizzbuzz/fizzbuzz.c
+++ fizzbuzz/fizzbuzz.c
@@ -10,14 +10,34 @@
 */
 int fizzbuzz(int n)
 {
-
+  int counter = 0;
+  for (int i = 0; i <= n; i++)
+  {
+    if (i % 3 == 0 && i % 5 == 0)
+    {
+      printf("fizzbuzz \n");
+    }
+    else if (i % 5 == 0)
+    {
+      printf("buzz \n");
+    }
+    else if (i % 3 == 0)
+    {
+      printf("fizz \n");
+    }
+    else
+    {
+      counter++;
+    }
+  }
+  return counter;
 }
 
 #ifndef TESTING
 int main(void)
 {
-    fizzbuzz(20);
+  fizzbuzz(20);
 
-    return 0;
+  return 0;
 }
 #endif
